8856868dde0c3ef6518837e049aa37cf03f42dc3
AdhamZahran158 <adhamzahranil123@yahoo.com>
2025-11-16 04:06:27 +0200

    2nd sub (not done)

--- Task13_v2/Task13_v2/Controllers/MovieController.cs
+++ Task13_v2/Task13_v2/Controllers/MovieController.cs
@@ -1,12 +1,88 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Task13.DataAccess;
+using Task13_v2.ViewModels;
 
 namespace Task13.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult Index()
+        ApplicationDbContext db = new(); 
+
+        public IActionResult MovieList()
         {
-            return View();
+            var movies = db.movies.AsQueryable();
+            var moviesWithCat = movies.Join(db.categories,m => m.CategoryId, c=> c.Id,(m,c)=> new
+            {
+               categoryName=c.Name,
+               movieName = m.Name,
+               moviePrice = m.Price,
+               movieStatus = m.Status,
+               movieDate = m.CreatedDate,
+               movieDescription = m.Description,
+               movieMainImg = m.MainImg
+               
+            });
+            return View(moviesWithCat.AsEnumerable());
+        }
+        [HttpGet]
+        public IActionResult CreateMovie()
+        {
+            var categories = db.categories.AsQueryable();
+            var cinema = db.cinema.AsQueryable();
+            ViewBag.cinema = cinema.AsEnumerable();
+            return View(categories.AsEnumerable());
+        }
+        [HttpPost]
+        public IActionResult CreateMovie(MovieVM movieVM, IFormFile mainImg, List<IFormFile> subImgs)
+        {
+            if (movieVM != null)
+            {
+                if (mainImg != null && mainImg.Length>0)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(mainImg.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images\\MoviesMainImg",fileName);
+                    if (!System.IO.File.Exists(filePath))
+                    {
+                        var stream = System.IO.File.Create(filePath);
+                        mainImg.CopyTo(stream);
+                    }
+                    movieVM.MainImg = fileName;
+                }
+                var newMovie = db.movies.Add(new()
+                {
+                    Name = movieVM.Name,
+                    MainImg = movieVM.MainImg,
+                    Price = movieVM.Price,
+                    CategoryId = movieVM.CatId,
+                    CinemaId = movieVM.CinemaId,
+                    Description = movieVM.Description,
+                    CreatedDate = movieVM.Date,
+                    Status = movieVM.Status
+                });
+                db.SaveChanges();
+                if (subImgs != null && subImgs.Count > 0)
+                {
+                    foreach (var item in subImgs)
+                    {
+                        var fileName = Guid.NewGuid().ToString() + Path.GetExtension(item.FileName);
+                        var filePath = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot\\Images\\MoviesSubImg",fileName);
+                        if (!System.IO.File.Exists(filePath))
+                        {
+                            var stream = System.IO.File.Create(filePath);    
+                            item.CopyTo(stream);
+                        }
+                        db.sub_images.Add(new()
+                        {
+                            Img = fileName,
+                            MovieId = newMovie.Entity.Id,
+                            Title = $"Movie {newMovie.Entity.Name} Photo"
+                        });
+                        db.SaveChanges();
+                    }
+                }
+            }
+            return RedirectToAction(nameof(MovieList));
         }
     }
 }
