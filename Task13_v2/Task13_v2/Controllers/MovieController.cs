28b48ce9e16dddc0344e6d54492ef8fe2443dae6
AdhamZahran158 <adhamzahranil123@yahoo.com>
2025-11-16 23:02:18 +0200

    third sub

--- Task13_v2/Task13_v2/Controllers/MovieController.cs
+++ Task13_v2/Task13_v2/Controllers/MovieController.cs
@@ -12,17 +12,20 @@ namespace Task13.Controllers
         public IActionResult MovieList()
         {
             var movies = db.movies.AsQueryable();
-            var moviesWithCat = movies.Join(db.categories,m => m.CategoryId, c=> c.Id,(m,c)=> new
-            {
-               categoryName=c.Name,
-               movieName = m.Name,
-               moviePrice = m.Price,
-               movieStatus = m.Status,
-               movieDate = m.CreatedDate,
-               movieDescription = m.Description,
-               movieMainImg = m.MainImg
-               
-            });
+            var moviesWithCat = db.movies.Join(db.categories,
+           m => m.CategoryId,
+           c => c.Id,
+           (m, c) => new MovieListVM
+           {
+               CategoryName = c.Name,
+               MovieName = m.Name,
+               MoviePrice = m.Price,
+               MovieStatus = m.Status,
+               MovieDate = m.CreatedDate,
+               MovieDescription = m.Description,
+               MovieMainImg = m.MainImg,
+               MovieId = m.Id
+           });
             return View(moviesWithCat.AsEnumerable());
         }
         [HttpGet]
@@ -34,7 +37,7 @@ namespace Task13.Controllers
             return View(categories.AsEnumerable());
         }
         [HttpPost]
-        public IActionResult CreateMovie(MovieVM movieVM, IFormFile mainImg, List<IFormFile> subImgs)
+        public IActionResult CreateMovie(MovieVM movieVM, IFormFile? mainImg, List<IFormFile>? subImgs)
         {
             if (movieVM != null)
             {
@@ -82,6 +85,84 @@ namespace Task13.Controllers
                     }
                 }
             }
+            return RedirectToAction(nameof(MovieList));
+        }
+
+        [HttpGet]
+        public IActionResult EditMovie(int id)
+        {
+            var movie = db.movies.FirstOrDefault(m => m.Id == id);
+            var categories = db.categories.AsNoTracking().AsEnumerable().ToList();
+            var cinema = db.cinema.AsNoTracking().AsEnumerable().ToList();
+            return View(new EditMovieVM
+            {
+                Movie = movie,
+                Categories = categories,
+                Cinema = cinema
+            });
+        }
+        [HttpPost]
+        public IActionResult EditMovie(int id,MovieVM movieVM, IFormFile? mainImg, List<IFormFile>? subImgs)
+        {
+            var movie = db.movies.FirstOrDefault(m => m.Id ==id);
+            var subImgsInDb = db.sub_images.AsQueryable();
+            var subImgsInSystem = subImgsInDb.Where(s => s.MovieId == movie.Id);
+            if (movie == null) {
+                return NotFound();
+            }
+            if (mainImg is not null && mainImg.Length> 0)
+            {
+                var fileName = Guid.NewGuid().ToString()+Path.GetExtension(mainImg.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images\\MoviesMainImg", fileName);
+                var oldPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images\\MoviesMainImg", movie.MainImg);
+
+                if(!System.IO.File.Exists(filePath))
+                {
+                    System.IO.File.Create(filePath);
+                    System.IO.File.Delete(oldPath);
+                }
+                movie.MainImg = fileName;
+                db.SaveChanges();
+            }
+            if(subImgs != null && subImgs.Count > 0)
+            {
+                foreach (var item in subImgsInSystem)
+                {
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images\\MoviesSubImg", item.Img);
+                    if (System.IO.File.Exists(filePath))
+                        System.IO.File.Delete(filePath);
+                    db.sub_images.Remove(item);
+                }
+                db.SaveChanges();
+                foreach (var item in subImgs)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(item.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Images\\MoviesSubImg", fileName);
+                    if(!System.IO.File.Exists (filePath))
+                    {
+                        System.IO.File.Create(filePath);
+                    }
+                    db.sub_images.Add(new()
+                    {
+                        Img = fileName,
+                        MovieId = movie.Id,
+                        Title = $"Movie {movie.Name} Photo"
+                    });
+                    db.SaveChanges();
+                }
+            }
+            if(movieVM is not null)
+            {
+                movie.Name = movieVM.Name;
+                movie.Description= movieVM.Description is null || movieVM.Description == "" ? movie.Description: movieVM.Description;
+                movie.CategoryId = movieVM.CatId;
+                movie.CinemaId = movieVM.CinemaId;
+                movie.Price = movieVM.Price;
+                movie.Status = movieVM.Status;
+                movie.CreatedDate = movieVM.Date;
+                db.SaveChanges();
+            }
+
             return RedirectToAction(nameof(MovieList));
         }
     }
--- Task13_v2/Task13_v2/obj/Debug/net9.0/Task13_v2.AssemblyInfo.cs
+++ Task13_v2/Task13_v2/obj/Debug/net9.0/Task13_v2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task13_v2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc2b98c80bb4e31ec404f40639d1f0b6332a3ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8856868dde0c3ef6518837e049aa37cf03f42dc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task13_v2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task13_v2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
